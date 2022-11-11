bcaa4155763b0dffa6abf7b5fb9f4bf21276a5ba
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-11-11 13:30:30 -0600

    User schema

---
+++ src/main/java/com/felipe/jwtAuthTest/entities/Role.java
@@ -0,0 +1,33 @@
+package com.felipe.jwtAuthTest.entities;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(name = "permissionsPerRole")
+    private List<Permission> permissions;
+
+    private String name;
+}
