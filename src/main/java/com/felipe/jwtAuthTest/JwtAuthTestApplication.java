18098569ec337473f82599a557f7f608fdbccc13
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-11-11 10:17:52 -0600

    Initial commit

---
+++ src/main/java/com/felipe/jwtAuthTest/JwtAuthTestApplication.java
@@ -0,0 +1,12 @@
+package com.felipe.jwtAuthTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/felipe/jwtAuthTest/JwtAuthTestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.felipe.jwtAuthTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthTestApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
