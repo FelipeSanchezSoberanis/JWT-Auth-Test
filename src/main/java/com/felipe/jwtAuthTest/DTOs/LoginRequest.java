6b1183f4c322a16293ddb2b0f1ef056130834911
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-11-14 12:24:26 -0600

    Login request object

---
+++ src/main/java/com/felipe/jwtAuthTest/DTOs/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.felipe.jwtAuthTest.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+
+    private String username;
+    private String password;
+}
